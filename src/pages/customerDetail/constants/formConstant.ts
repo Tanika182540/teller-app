190dee0f08b1b5601091a87189b0e06010af7fe8
Tanika Kulchutisin <tanika.k@bitazza.com>
2025-03-30 01:47:51 +0700

    feat: prepare form validation

---
+++ src/pages/customerDetail/constants/formConstant.ts
@@ -0,0 +1,11 @@
+export const FormStep = {
+  STEP1_CUSTOMER_DETAIL: 1,
+  STEP2_VALIDATE_DOCUMENT: 2,
+  STEP3_DOCUMENT_REVIEW: 3,
+};
+
+export const FormTitle = [
+  "Customer Detail",
+  "Document Verification",
+  "Review Information",
+];
---
+++ src/shared/regex/validationRules.ts
@@ -0,0 +1,4 @@
+export const ValidationRules = {
+  nationalIdNumber: /^\d{13}$/,
+  StringsExceptNumber:/^[A-Za-z]+$/,
+};
