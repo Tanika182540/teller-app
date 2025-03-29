2c53bcde5a9b9c76368dcb3d350f314e365a4dce
Tanika Kulchutisin <tanika.k@bitazza.com>
2025-03-30 01:50:47 +0700

    feat: create Not Found Page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
-import Input from "../shared/components/Input.component";
-
-
 export default function NotFound() {
-  const [email, setEmail] = useState<string>("");
   return (
     <div className="flex flex-col items-center justify-center h-full w-full">
-      <Input title="Email" value={email} onChange={setEmail} />
+      {/* TODO: Ui implement */}
+      Page Not Found!
     </div>
   );
 }
