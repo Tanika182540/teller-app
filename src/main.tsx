ebb0116300ada1e12e9ae764763f05526f6c7bed
Tanika Kulchutisin <tanika.k@bitazza.com>
2025-03-30 11:48:08 +0700

    feat: project setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/login/Login";
 import NotFound from "./pages/NotFound";
 import "./index.css";
 import CustomerDetails from "./pages/customerDetail/CustomerDetails";
+import { AuthProvider, useAuth } from "./routes/AuthContext";
+import { Toaster } from "react-hot-toast";
 import Layout from "./shared/components/Layout";
+
+const App = () => {
+  const { user } = useAuth();
+  const isAuthenticated = !!user;
+
+  return (
+    <Routes>
+      {/* Protected Routes (Require Authentication) */}
+      {isAuthenticated ? (
+        <Route element={<Layout />}>
+          <Route path="/customer-details" element={<CustomerDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      ) : (
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      )}
+
+      {/* Public Routes */}
+      <Route
+        path="/login"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/customer-details" replace />
+          ) : (
+            <Login />
+          )
+        }
+      />
+    </Routes>
+  );
+};
+
+export default App;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <AuthProvider>
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/customer-details" element={<CustomerDetails />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
+        <App />
       </BrowserRouter>
+    </AuthProvider>
+    <Toaster />
   </React.StrictMode>
 );
